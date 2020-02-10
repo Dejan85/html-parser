ac7a3171ad098a7093e63f9379cd505f224aa69d
Dejan85 <xypnotica@gmail.com>
2020-02-10 21:50:16 +0100

    izvukao samo linkove

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import htmlParser from "html-parser";
@@ -6,33 +6,43 @@ import { xad } from "./jsonFake";
 
 // https://www.npmjs.com/package/html-parser
 
+// console.log(JSON.parse(xad));
+
+const reg = `https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)`;
+
 function App() {
+  const [test, setTest] = useState();
+
   var html = xad;
   htmlParser.parse(html, {
-    openElement: function(name) {
-      console.log("open: %s", name);
-    },
-    closeOpenedElement: function(name, token, unary) {
-      console.log("token: %s, unary: %s", token, unary);
-    },
-    closeElement: function(name) {
-      console.log("close: %s", name);
-    },
-    comment: function(value) {
-      console.log("comment: %s", value);
-    },
-    cdata: function(value) {
-      console.log("cdata: %s", value);
-    },
+    // openElement: function(name) {
+    //   console.log("open: %s", name);
+    // },
+    // closeOpenedElement: function(name, token, unary) {
+    //   console.log("token: %s, unary: %s", token, unary);
+    // },
+    // closeElement: function(name) {
+    //   console.log("close: %s", name);
+    // },
+    // comment: function(value) {
+    //   console.log("comment: %s", value);
+    // },
+    // cdata: function(value) {
+    //   console.log("cdata: %s", value);
+    // },
     attribute: function(name, value) {
-      console.log("attribute: %s=%s", name, value);
-    },
-    docType: function(value) {
-      console.log("doctype: %s", value);
-    },
-    text: function(value) {
-      console.log("text: %s", value);
+      // console.log("attribute: %s=%s", name, value);
+      // console.log(value.match(reg));
+      if (name === "data-href") {
+        console.log(value);
+      }
     }
+    // docType: function(value) {
+    //   console.log("doctype: %s", value);
+    // },
+    // text: function(value) {
+    //   console.log("text: %s", value);
+    // }
   });
 
   return (
