6e7bb03c1de8d10837994bf14a58a8894ae7b30b
Dejan85 <xypnotica@gmail.com>
2020-02-10 21:29:49 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import htmlParser from "html-parser";
+import { xad } from "./jsonFake";
 
 function App() {
+  var html = xad;
+  htmlParser.parse(html, {
+    openElement: function(name) {
+      console.log("open: %s", name);
+    },
+    closeOpenedElement: function(name, token, unary) {
+      console.log("token: %s, unary: %s", token, unary);
+    },
+    closeElement: function(name) {
+      console.log("close: %s", name);
+    },
+    comment: function(value) {
+      console.log("comment: %s", value);
+    },
+    cdata: function(value) {
+      console.log("cdata: %s", value);
+    },
+    attribute: function(name, value) {
+      console.log("attribute: %s=%s", name, value);
+    },
+    docType: function(value) {
+      console.log("doctype: %s", value);
+    },
+    text: function(value) {
+      console.log("text: %s", value);
+    }
+  });
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/jsonFake.js
@@ -0,0 +1,27 @@
+const x = {
+  article:
+    '<p>Foxconn, one of Apple&#39;s biggest suppliers, told its employees on Friday not to return to work even after the extended Lunar New Year holiday ends on February 10 amid increasing fears of the novel coronavirus spreading.</p>\n\n<p>According to earlier reports, Foxconn is now trying to switch to <span class="ttNewsSelectForSpan" data-href="http://www.teletrader.com/foxconn-makes-masks-in-rush-to-restart-lines-in-china-report/news/details/51189710" data-news-link="51189710">mask production</span> in order to restart its factories in China.</p>\n\n<p>In a memo obtained by Bloomberg, advised its staff to keep away from the Shenzhen plant. &quot;We&#39;ll update you later,&quot; the memo added cryptically.</p>\n\n<p>An <span class="ttNewsSelectForSpan" data-href="http://www.teletrader.com/hyundai-kia-at-risk-of-heavy-output-losses-due-to-coronavirus/news/details/51197299" data-news-link="51197299">increasing</span> <span class="ttNewsSelectForSpan" data-href="http://www.teletrader.com/fiat-chryslers-european-plant-at-risk-due-to-coronavirus/news/details/51188437" data-news-link="51188437">number</span> of companies continue to shut down their operations around the world as the outbreak of the deadly disease continues to weigh on the <span class="ttNewsSelectForSpan" data-href="http://www.teletrader.com/us-stocks-down-in-premarket-as-coronavirus-fears-rise/news/details/51196726" data-news-link="51196726">stock</span> and <span class="ttNewsSelectForSpan" data-href="http://www.teletrader.com/oil-prices-fall-with-opec-yet-to-agree-on-output-cuts/news/details/51188331" data-news-link="51188331">commodity</span> markets worldwide.</p>',
+  headline:
+    "Apple supplier tells staff not to return to work after holiday break",
+  newsSource: "TeleTrader",
+  dateTime: "2020-02-07T14:03:00",
+  linkedSymbols: [
+    {
+      isin: "US4380908057"
+    },
+    {
+      name: "Apple Inc.",
+      isin: "US0378331005"
+    }
+  ],
+  tags: {
+    imageData: {
+      imageId: "165934",
+      imageSource: "EPA-EFE/MAHMOUD KHALED"
+    },
+    notificationDateTime: "0001-01-01T00:00:00",
+    marketImpact: null
+  }
+};
+
+export const xad = JSON.stringify(x);
